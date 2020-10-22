0a01da08c2c4707a6e480c0f7d196845e91bf30d
dmitry fedorenko <dmitry.fedorenko@crypterium.com>
2020-10-22 08:54:52 +0300

    Initial commit

---
+++ lib/App.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import 'MyText.dart';
+import 'TextControl.dart';
+
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  String _text = "This is my first assignment";
+
+  void _changeText() {
+    setState(() {
+      _text = "Completed";
+      print("_changeText called");
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(title: Text("First assignment")),
+          body: Column(
+            children: [
+              MyText(_text),
+              TextControl(_changeText)
+            ],
+          )),
+    );
+  }
+}
